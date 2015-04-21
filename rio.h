e163530cbc644a5c12aa24e3e0d69bff6200afd2
swn73 <hua_lin@live.cn>
2015-04-21 21:36:32 +0800

    add new file

---
+++ rio.h
@@ -0,0 +1,108 @@
+//
+// Created by root on 15-4-21.
+//
+
+#ifndef TEST_PROJECT_RIO_H
+#define TEST_PROJECT_RIO_H
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+/*
+ *  IO function of read() and write() without buffer.
+ */
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+
+ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = (char *) usrbuf;
+    while (nleft > 0) {
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) {   //  will call read() again.
+                nread = 0;  /*  Interrupted by  sig handler return  */
+            }
+            else {
+                return -1;  /*  errno set by read() */
+            }
+        } else if (0 == nread) {
+            break;  // EOF
+        }
+        nleft -= nread;
+        bufp += nread;
+    }
+    return n - nleft;
+}
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nwriten;
+    char *bufp = (char *) usrbuf;
+    while (nleft > 0) {
+        if ((nwriten = write(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) {
+                nwriten = 0;
+            }
+            else {
+                return -1;
+            }
+        }
+        nleft -= nwriten;
+        usrbuf += nwriten;
+    }
+    return n;
+}
+
+
+/*
+ *  IO function with buffer.
+ */
+#define RIO_BUFFER_SIZE 8192
+typedef struct {
+    int rio_fd;
+    int rio_cnt;
+    char *rio_bufptr;
+    char rio_buf[RIO_BUFFER_SIZE];
+} rio_t;
+
+void rio_readinitb(rio_t *rp, int fd);
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t n);
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n);
+
+void rio_readinitb(rio_t *rp, int fd) {
+    rp->rio_fd = fd;
+    rp->rio_cnt = 0;
+    rp->rio_bufptr = rp->rio_buf;
+}
+
+
+ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {
+    int cnt;
+    while (rp->rio_cnt <= 0) {
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, RIO_BUFFER_SIZE);
+        if (rp->rio_cnt < 0) {
+            if (errno != EINTR) {
+                return -1;
+            }
+        } else if (0 == rp->rio_cnt) {
+            return 0;
+        } else {
+            rp->rio_bufptr = rp->rio_buf;
+        }
+    }
+
+    cnt = n < (rp->rio_cnt) ? n : rp->rio_cnt;
+    memcpy(usrbuf, rp->rio_bufptr, cnt);
+    rp->rio_bufptr += cnt;
+    rp->rio_cnt -= cnt;
+    return cnt;
+}
+
+#endif //TEST_PROJECT_RIO_H
