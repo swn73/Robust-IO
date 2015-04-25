7617a0fe8dbd69587a9c87faec9ae44578359864
swn73 <hua_lin@live.cn>
2015-04-25 19:41:58 +0800

    add some function

--- rio.h
+++ rio.h
@@ -70,9 +70,13 @@ typedef struct {
 
 void rio_readinitb(rio_t *rp, int fd);
 
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+
+ssize_t rio_writelineb(rio_t *p, void *usrbuf, size_t n);
 
 ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+
+ssize_t rio_writenb(rio_t *p, void *usrbuf, size_t n);
 
 static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n);
 
@@ -103,6 +107,50 @@ ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {
     rp->rio_bufptr += cnt;
     rp->rio_cnt -= cnt;
     return cnt;
+}
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+    int n, rc;
+    char c, *bufp = usrbuf;
+    for (n = 1; n < maxlen; ++n) {
+        if ((rc = rio_read(rp, &c, 1)) == 1) {
+            *bufp = c;
+            bufp++;
+            if ('\n' == c) {
+                break;
+            }
+        }
+        else if (0 == rc) {
+            if (1 == n)
+                return 0;
+            else
+                break;
+        } else {
+            return -1;
+        }
+    }
+    *bufp = '\0';
+    return n;
+}
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n) {
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+    while (nleft > 0) {
+        if ((nread = rio_read(rp, bufp, nleft)) < 0) {
+            if (errno == EINTR)
+                nread = 0;
+            else
+                return -1;
+        }
+        else if (0 == nread) {
+            break;
+        }
+        nleft = nleft - nread;
+        bufp = bufp + nread;
+    }
+    return n - nleft;
 }
 
 #endif //TEST_PROJECT_RIO_H
